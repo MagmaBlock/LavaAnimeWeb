dd68b7f592eac4616fd6ebaa41719b64f1ff7889
Ming <3046652718@qq.com>
2022-05-04 21:52:25 +0800

    优化滚动条；移除侧边搜索

--- js/Navbar.js
+++ js/Navbar.js
@@ -96,7 +96,7 @@ $(document).ready(() => {
     const toolBar =
         `<div class="toolbar">
             <div class="toolButton goTopButton" style="opacity: 0;"><i class="bi bi-arrow-up-circle fs-5"></i></div>
-            <div class="toolButton searchButton"><span><i class="bi bi-search fs-5"></i></span><input class="form-control" placeholder="搜索 番剧名称 或 Bangumi ID"></div>
+            <!-- <div class="toolButton searchButton"><span><i class="bi bi-search fs-5"></i></span><input class="form-control" placeholder="搜索 番剧名称 或 Bangumi ID"></div> -->
         </div>`
     const icon = {
         home: "bi-house",
